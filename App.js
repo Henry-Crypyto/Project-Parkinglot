74855704633a2f7d8f706c0860ca483245ca60e7
Henry <hjinshan0813@gmail.com>
2022-06-28 12:42:31 +0800

    draw line on image complete

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View ,SafeAreaView,Image,Animated} from 'react-native';
+import { StyleSheet, Text, View ,SafeAreaView,Image,Animated,ImageBackground} from 'react-native';
 import { PinchGestureHandler } from "react-native-gesture-handler";
 import Svg, {
   Circle,
@@ -26,9 +26,20 @@ export default function App() {
   const scale = React.useRef(new Animated.Value(1)).current;
   const handlePinch=Animated.event([{nativeEvent:{scale}}], {useNativeDriver: true},);
   return (
-    <PinchGestureHandler onGestureEvent={handlePinch}>
-      <Animated.Image source = {require('./parkingLot.jpg')} style={[styles.img,{transform:[{scale}]}]}/>
-    </PinchGestureHandler>
+    
+      <ImageBackground source = {require('./parkingLot.jpg')} style={styles.img}>
+      <Svg>
+         <Line x1="100" y1="200" x2="270" y2="200" stroke="red" strokeWidth="2" />
+         <Line x1="100" y1="115" x2="100" y2="200" stroke="red" strokeWidth="2" /> 
+         <Line x1="270" y1="80" x2="270" y2="200" stroke="red" strokeWidth="2" />  
+         <Line x1="270" y1="80" x2="300" y2="80" stroke="red" strokeWidth="2" />
+         <Line x1="80" y1="115" x2="100" y2="115" stroke="red" strokeWidth="2" />
+
+      </Svg> 
+      
+      </ImageBackground>
+       
+   
   ); 
 }
 
@@ -41,12 +52,14 @@ const styles = StyleSheet.create({
   },
   img: {
     flex: 1,
-    height: null,
-    resizeMode: 'stretch',
-    width:null,
+    height: 300,
+    resizeMode: 'contain',
+    width:380,
     justifyContent:"center",    
     //transform: [{ rotate: '90deg' }],
-    // aspectRatio: 1
+    aspectRatio: 1,
+    position: 'absolute', 
+    bottom: 300
   }
 
 }); 
